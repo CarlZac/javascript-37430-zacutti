0033f0f338b2ae79107f2adc400f3a5c72845ed2
CarlosZacutti <czacutti@gmail.com>
2022-05-15 21:10:22 -0300

    Anexo de opciones 4 y 5

--- js/script.js
+++ js/script.js
@@ -8,10 +8,16 @@ function suma (num1, num2) {
 function resta (num1, num2) {
     resultadoResta = num1 - num2;
 }
+function multi(num1, num2) {
+    resultadoMulti = num1 * num2;
+}
+function divi(num1, num2) {
+    resultadoDivi = num1 / num2;
+}
 
 do {
-    userOption = prompt(userName + ' elegí una opción:\n1- Tu edad en 10 años.\n2- Tu edad hace 5 años.\n3- Cuánto te falta para los 50 años.\nEscribe ESC para salir.');
-    if (userOption != '1' && userOption != '2' && userOption != '3' && userOption != 'ESC'){
+    userOption = prompt(userName + ' elegí una opción:\n1- Tu edad en 10 años.\n2- Tu edad hace 5 años.\n3- Cuánto te falta para los 50 años.\n4- Tu edad si tuvieras el doble de años.\n5- La edad que tendrías de tener la mitad de años.\nEscribe ESC para salir.');
+    if (userOption != '1' && userOption != '2' && userOption != '3' && userOption != '4' && userOption != '5' && userOption != 'ESC'){
         alert('Entrada Inválida');
         continue;
     }
@@ -37,6 +43,17 @@ do {
                 alert('Para que cumplas los 50 te pasaste por ' + agePastFifty + ' años.');
             }
             break;
+        case '4':
+            multi(userAge, 2);
+            let ageByTwo = resultadoMulti;
+            alert(userName + ' si tuvieras el doble de años, tendrías ' + ageByTwo);
+            break;
+        case '5':
+            divi(userAge, 2);
+            let ageInHalf = resultadoDivi;
+            alert(userName + ' si tuvieras la mitad de años, tendrías ' + ageInHalf);
+            break;
+
         default:
             break;
     }
